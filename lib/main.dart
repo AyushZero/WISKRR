dea423eea1cedee0ae724bf445c913e208d4afd2
Ayush Singh <ayushsingh30082005@gmail.com>
2025-06-07 16:10:31 +0530

    Initial Commit.

--- lib/main.dart
+++ lib/main.dart
@@ -20,61 +20,23 @@ class MyApp extends StatelessWidget {
           height: 956,
           clipBehavior: Clip.antiAlias,
           decoration: BoxDecoration(color: const Color(0xFFFFF3E1)),
-          child: Stack(
-            children: [
-              Positioned(
-                left: 149.25,
-                top: 944.50,
-                child: Container(
-                  width: 124.50,
-                  height: 7,
-                  decoration: ShapeDecoration(
-                    color: const Color(0xFFD9D9D9),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                ),
+          child: Center(
+            child: Container(
+              width: 216,
+              height: 216,
+              decoration: ShapeDecoration(
+                 image: const DecorationImage(
+                    image:  AssetImage("assets/wiskrr.png"),
+                    fit: BoxFit.cover,
+                 ),
+                 shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(150),
+                 ),
               ),
-              Positioned(
-                left: 112,
-                top: 370,
-                child: Container(
-                  width: 216,
-                  height: 216,
-                  decoration: ShapeDecoration(
-                    image: const DecorationImage(
-                      image:  AssetImage("assets/wiskrr.png"),
-                      fit: BoxFit.cover,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(150),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 0,
-                top: 0,
-                child: Container(
-                  width: 440,
-                  height: 30,
-                  decoration: ShapeDecoration(
-                    color: const Color(0xFFFFF3E1),
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        width: 1,
-                        strokeAlign: BorderSide.strokeAlignOutside,
-                        color: const Color(0xFF5D4016),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
