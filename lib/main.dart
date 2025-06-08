20ac8287ebb604488f0c84be89354f29319706fb
Ayush Singh <ayushsingh30082005@gmail.com>
2025-06-08 15:01:33 +0530

    In development.

--- lib/main.dart
+++ lib/main.dart
@@ -14,29 +14,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFFF3E1),
-      body: Center(
-        child: Container(
-          width: 440,
-          height: 956,
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(color: const Color(0xFFFFF3E1)),
-          child: Center(
-            child: Container(
-              width: 216,
-              height: 216,
-              decoration: ShapeDecoration(
-                 image: const DecorationImage(
-                    image:  AssetImage("assets/wiskrr.png"),
-                    fit: BoxFit.cover,
-                 ),
-                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(150),
-                 ),
-              ),
+        body: Center(
+          child: Container(
+            width: 440,
+            height: 956,
+            clipBehavior: Clip.antiAlias,
+            decoration: BoxDecoration(color: const Color(0xFFFFF3E1)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 150), // <-- vertical position of the image
+                Container(
+                  width: 156,
+                  height: 156,
+                  decoration: ShapeDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage("assets/wiskrr.png"),
+                      fit: BoxFit.cover,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(150),
+                    ),
+                  ),
+                ),
+                Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: const Color(0xFF5C4015),
+                    fontSize: 30,
+                    fontFamily: 'Young Serif',
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
             ),
           ),
-        ),
-      )
+        )
+
     );
   }
 }
